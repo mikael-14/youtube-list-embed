5a2f6b3c0745b4433d90b7ac0781ff08ef935ae9
Mickael Marques <43293955+mikael-14@users.noreply.github.com>
2022-07-25 11:01:37 +0100

    Create example.js
    Initial commit

---
+++ example.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<script src="https://yvoschaap.com/ytpage/ytembed.js"></script>
+<div id="ytThumbs"><ins>Made with the <a href="https://yvoschaap.com/youtube.html?utm_source=generator">Youtube Embed Generator</a>.</ins></div>
+
+<script>
+	ytEmbed.init({'block':'ytThumbs','key':'YOUR_KEY',type':'user','results':5,'meta':true,'player':'embed','layout':'full'});
+</script>
+
+</body>
+</html>
